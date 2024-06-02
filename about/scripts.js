c105a9f847667080671650a4e04fadfd4a9ae96a
DaIsimsiz <daisimsiz.tr@gmail.com>
2024-06-02 23:02:41 +0300

    i dont have to write informative desc's if its my personal projecT!!!!!

---
+++ about/scripts.js
@@ -0,0 +1,4 @@
+﻿window.onscroll = function (e) {
+    var body = document.body;
+    //body.style.backgroundPositionY = (45 - ((scrollY / window.innerHeight) * (1440 - 309 - window.outerHeight))) + "px";
+} 
